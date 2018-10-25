894c645ba6000b6bd512d6f1ae19b4019b5e93bb
Xavier Yao <xavieryao@live.cn>
2018-10-25 14:02:51 +0800

    update/get/delete intent

--- apis/intent.py
+++ apis/intent.py
@@ -36,3 +36,31 @@ def create_intent():
         tree=body['tree']
     ).save()
     return jsonify(intent)
+
+@intent_apis.route('/<intent_id>', methods=['GET', 'PUT', 'DELETE'])
+def route_single_entity(agent_id, intent_id):
+    intent = Intent.objects.get(id=intent_id)
+    assert str(intent.agent.id) == agent_id
+    if request.method == 'GET':
+        return get_intent(intent)
+    elif request.method == 'PUT':
+        return update_intent(intent)
+    elif request.method == 'DELETE':
+        return delete_intent(intent)
+
+def get_intent(intent):
+    return jsonify(intent.to_view())
+
+def update_intent(intent):
+    body = request.get_json()
+    if 'name' in body:
+        intent.name = body['name']
+    if 'description' in body:
+        intent.description = body['description']
+    if 'tree' in body:
+        intent.tree = body['tree']
+    return jsonify(intent.to_view())
+
+def delete_intent(intent):
+    intent.delete()
+    return 'done'
