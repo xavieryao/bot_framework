273ea23ca6439320bf70a60d766de3f41b9b3d3a
Xavier Yao <xavieryao@live.cn>
2018-11-28 13:54:15 +0800

    call sent simulator

--- learnwares/sentence_simulator.py
+++ learnwares/sentence_simulator.py
@@ -2,8 +2,13 @@ from models.intent import Intent
 from models.entity import Entity
 from models.agent import Agent
 import json
+import subprocess
+import os
 
 class SentenceGenerator:
+    SENTENCE_GENERATOR_PATH = '~/keg/sentence_simulator/main.py'
+    SENT_COUNT = 100
+
     def __init__(self, agent):
         self.agent = agent
 
@@ -25,8 +30,35 @@ class SentenceGenerator:
             entity_dict = entity.to_view()
             del entity_dict['description']
             del entity_dict['agent_id']
+            if len(entity_dict['entries']) == 0:
+                entity_dict['entries'].append('PLACEHOLDER')
             rules['entity'].append(entity_dict)
         return rules
+
+    def generate_data(self):
+        data_path = "data/{}/data/".format(self.agent.id)
+        os.makedirs(data_path)
+        rules = self.generate_rules()
+
+        rules_path = os.path.join(data_path, "sent_sim_rules.json")
+        sent_path = os.path.join(data_path, "sents.txt")
+        word_path = os.path.join(data_path, "words.txt")
+
+        with open(rules_path, 'w') as f:
+            json.dump(rules, f)
+        cp = subprocess.run([
+            "python3",
+            self.SENTENCE_GENERATOR_PATH,
+            "-f",
+            rules_path,
+            "-c",
+            str(self.SENT_COUNT),
+            "-w",
+            word_path,
+            "-s",
+            sent_path
+        ])
+        cp.check_returncode()
 
 def test():
     for agent in Agent.objects:
